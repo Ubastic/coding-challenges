d051b29427510f5631b4e09c24971d0eea3f8f9a
KarabasYurii <1998uriyyo@gmail.com>
2019-10-21 09:29:18 +0300

    🐍 Update solution for "Sum of Intervals"

--- solutions/python/4 kyu/Sum of Intervals/solution.py
+++ solutions/python/4 kyu/Sum of Intervals/solution.py
@@ -1,11 +1,5 @@
+from itertools import chain
+
+
 def sum_of_intervals(intervals):
-    current, *intervals = map(list, sorted(intervals, key=lambda i: i[0]))
-    res = [current]
-
-    for start, end in filter(lambda a: res[-1][0] > a[0] or res[-1][1] < a[1], intervals):
-        if start <= res[-1][1] <= end:
-            res[-1][1] = end
-        else:
-            res.append([start, end])
-
-    return sum(b - a for a, b in res)
+    return len(set(chain.from_iterable(range(i, j) for i, j in intervals)))
