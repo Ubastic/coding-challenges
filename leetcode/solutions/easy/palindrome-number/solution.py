2cc7889bcb33f22e1652967cac9d2aee8bc6cf6d
KarabasYurii <1998uriyyo@gmail.com>
2019-12-14 10:00:17 +0200

    🐍 Update solution for "Palindrome Number"

--- leetcode/solutions/easy/palindrome-number/solution.py
+++ leetcode/solutions/easy/palindrome-number/solution.py
@@ -1,7 +1,3 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0:
-            return False
-
-        s = str(x)
-        return all(s[i] == s[- (i + 1)] for i in range(len(s) // 2)) or len(s) == 1
+        return str(x) == str(x)[::-1]
