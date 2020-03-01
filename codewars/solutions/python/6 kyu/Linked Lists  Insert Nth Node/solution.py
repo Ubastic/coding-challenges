102769b300e6baf0f5fb021391d13b7a8b55e0c9
KarabasYurii <1998uriyyo@gmail.com>
2020-03-01 13:12:18 +0200

    🐍 Add solution "Linked List Insert Nth Node"

--- codewars/solutions/python/6 kyu/Linked Lists  Insert Nth Node/solution.py
+++ codewars/solutions/python/6 kyu/Linked Lists  Insert Nth Node/solution.py
@@ -1,19 +1,8 @@
 def insert_nth(head, index, data):
-    if not head and index == 0:
-        return Node(data)
+    if not index:
+        n = Node(data)
+        n.next = head
+        return n
 
-    prev = None
-    node = head
-    for i in range(index):
-        assert node is not None
-        prev = node
-        node = node.next
-
-    n = Node(data)
-    if prev:
-        prev.next = n
-    else:
-        head = n
-    n.next = node
-
+    head.next = insert_nth(head.next, index - 1, data)
     return head
