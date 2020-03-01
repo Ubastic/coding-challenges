3f50d4f35e524181c73db9727c5e28928df90e70
KarabasYurii <1998uriyyo@gmail.com>
2020-03-01 13:15:20 +0200

    🐍 Add solution "Linked List Sorted Insert"

--- codewars/solutions/python/6 kyu/Linked Lists  Sorted Insert/solution.py
+++ codewars/solutions/python/6 kyu/Linked Lists  Sorted Insert/solution.py
@@ -1,20 +1,8 @@
 def sorted_insert(head, data):
-    if head is None:
-        return Node(data)
+    if head is None or head.data > data:
+        n = Node(data)
+        n.next = head
+        return n
 
-    node = head
-    prev = None
-    while node:
-        if node.data > data:
-            n = Node(data)
-            if prev is None:
-                head, n.next = n, head
-            else:
-                prev.next, n.next = n, node
-            break
-
-        prev, node = node, node.next
-    else:
-        prev.next = Node(data)
-
+    head.next = sorted_insert(head.next, data)
     return head
