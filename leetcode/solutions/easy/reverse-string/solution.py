6a16baa5cd671c81409e9d581e19b8e3202b8012
Yurii Karabas <1998uriyyo@gmail.com>
2020-06-04 11:45:30 +0300

    :snake: Update solution for "Reverse String"

--- leetcode/solutions/easy/reverse-string/solution.py
+++ leetcode/solutions/easy/reverse-string/solution.py
@@ -1,7 +1,9 @@
+import math
+
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         """
         Do not return anything, modify s in-place instead.
         """
-        for i in range(len(s) // 2):
+        for i in range(math.ceil(len(s) / 2)):
             s[i], s[-(i + 1)] = s[-(i + 1)], s[i]
